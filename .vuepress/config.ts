ce7cee5b839cd60641f98400ca506b98795be3de
reallyzzg <reallyzzg@qq.com>
2024-07-14 01:29:52 +0800

    update logo

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -4,15 +4,15 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
-  title: "zzgのblog",
+  title: "vuepress-theme-reco",
   description: "Just playing around",
   bundler: viteBundler(),
   // bundler: webpackBundler(),
   theme: recoTheme({
     style: "@vuepress-reco/style-default",
     logo: "/logo.png",
-    author: "zzg",
-    authorAvatar: "/head.jpg",
+    author: "reco_luan",
+    authorAvatar: "/head.png",
     docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
     docsBranch: "main",
     docsDir: "example",
@@ -32,8 +32,8 @@ export default defineUserConfig({
     },
     navbar: [
       { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/stm32/1" },
-      { text: "Tags", link: "/tags/uart/1" },
+      { text: "Categories", link: "/categories/reco/1/" },
+      { text: "Tags", link: "/tags/tag1/1/" },
       // {
       //   text: "Docs",
       //   children: [
@@ -42,60 +42,60 @@ export default defineUserConfig({
       //   ],
       // },
     ],
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: "text",
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "QQ 群",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "title",
-    //       content: "GitHub",
-    //     },
-    //     {
-    //       type: "text",
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: "font-size: 12px;",
-    //     },
-    //     {
-    //       type: "hr",
-    //     },
-    //     {
-    //       type: "buttongroup",
-    //       children: [
-    //         {
-    //           text: "打赏",
-    //           link: "/docs/others/donate.html",
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
+    bulletin: {
+      // body: [
+      //   {
+      //     type: "text",
+      //     content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "title",
+      //     content: "QQ 群",
+      //   },
+      //   {
+      //     type: "text",
+      //     content: `
+      //     <ul>
+      //       <li>QQ群1：1037296104</li>
+      //       <li>QQ群2：1061561395</li>
+      //       <li>QQ群3：962687802</li>
+      //     </ul>`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "title",
+      //     content: "GitHub",
+      //   },
+      //   {
+      //     type: "text",
+      //     content: `
+      //     <ul>
+      //       <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+      //       <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+      //     </ul>`,
+      //     style: "font-size: 12px;",
+      //   },
+      //   {
+      //     type: "hr",
+      //   },
+      //   {
+      //     type: "buttongroup",
+      //     children: [
+      //       {
+      //         text: "打赏",
+      //         link: "/docs/others/donate.html",
+      //       },
+      //     ],
+      //   },
+      // ],
+    },
     // commentConfig: {
     //   type: 'valine',
     //   // options 与 1.x 的 valineConfig 配置一致
